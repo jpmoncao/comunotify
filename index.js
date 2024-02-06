f4d10c5c846f478a6773ade6bd12c93a679002a0
jpmoncao <pedrohjoao44@gmail.com>
2024-02-06 14:52:55 -0300

    feat: login

--- index.js
+++ index.js
@@ -2,17 +2,53 @@ import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 import { uuid } from 'uuidv4';
+import cors from 'cors';
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
 app.use(express.static('public'));
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (req, res) => res.sendFile('index.html'))
+// Views
+app.get('/', (req, res) => res.sendFile('index.html'));
+app.get('/chat', (req, res) => res.sendFile('../chat.html'));
+
+// Endpoints
+app.post('/login', (req, res) => {
+    let { name, group } = req.body;
+
+    switch (group) {
+        case 1:
+            group = 'telao';
+            break;
+        case 2:
+            group = 'salinha';
+            break;
+        case 3:
+            group = 'estacionamento';
+            break;
+
+        default:
+            group = null;
+            break;
+    }
+
+    if (group)
+        return res.status(201).json({
+            message: 'Login efetuado com sucesso!',
+            data: { name, group }
+        });
+
+    return res.status(500).json({
+        message: 'Erro ao efetuar login!',
+        data: null
+    });
+})
 
 io.on('connection', (socket) => {
-    console.log(socket.id + ' connected');
     io.emit('count', io.engine.clientsCount);
     io.engine.generateId(req => uuid.v4());
 
@@ -21,9 +57,8 @@ io.on('connection', (socket) => {
     })
 
     socket.on('disconnect', () => {
-        console.log(socket.id + ' disconnected');
         io.emit('count', io.engine.clientsCount);
     })
 });
 
-server.listen(5000);
+server.listen(5000, () => console.log('Server is running!'));
